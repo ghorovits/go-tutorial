c19ea33f475bb7d85bcd302336206d0d69f9a5e3
Gal <gal@madhive.com>
2023-10-20 11:08:05 -0400

    initial commit

---
+++ api/cex.go
@@ -0,0 +1,17 @@
+package api
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+
+	"frontendmasters.com/go/crypto/datatypes"
+)
+
+const apiUrl = "https://cex.io/api/ticker/%s/USD"
+
+func GetRate(currency string) (datatypes.Rate, error) {
+	upperCurrency := strings.ToUpper(currency)
+// make an http request
+	res, err := http.Get(fmt.Sprintf(apiUrl, upperCurrency))
+}
